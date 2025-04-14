f423a350f9407641f2e4ce84dc86b4ee5bfc05c0
krishnaswaroop616 <kswaroop311@gmail.com>
2025-04-14 17:20:31 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext=createContext(null);
 
 const StoreContextProvider=(props)=>{
 
-    const url="http://localhost:4000";
+    const url="https://deli-go-backend.onrender.com";
 
     const [token,setToken]=useState("");
 
